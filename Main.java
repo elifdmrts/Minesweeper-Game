f49523083a8677e137764064dc5c04f80a97af94
Elif Demirtaş <elif_demirtas_@outlook.com>
2023-11-24 00:12:25 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row, column;
+        System.out.println("Mayın Tarlasına Hoşgeldiniz!");
+        System.out.println("Lütfen oynamak istediğiniz boyutları giriniz !");
+        // System.out.print("Satır Sayısı: ");
+        //row = scanner.nextInt();
+        // System.out.print("Sütun Sayısı: ");
+        //  column = scanner.nextInt();
+        MayinTarlasi mayinTarlasi = new MayinTarlasi(4,3);
+        mayinTarlasi.run();
+
+    }
+}
